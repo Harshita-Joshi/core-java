9a8709cc823c1ed0ffa8729a07baf1bd748b0ec9
Harshita Joshi <68645591+Harshita-Joshi@users.noreply.github.com>
2022-02-20 13:02:07 +0530

    Data types in java

---
+++ Functions/DataType.java
@@ -0,0 +1,15 @@
+public class DataType {
+
+    static int a = 7;
+    static double d = 8.6;
+
+    public static void main(String[] args) {
+        int a = 6;
+        double d = 9.76;
+        char ch = 'a';
+        System.out.println(a);
+        System.out.println(d);
+        System.out.println(ch);
+    }
+
+}
